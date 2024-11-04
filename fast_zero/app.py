847a56b1e34f526ba0c3a8d350f2772301e3f017
Jean Alves de Oliveira <jeanalvesoliveira@gmail.com>
2024-11-04 08:30:51 -0300

    Explicita o codigo HTTP que deve retornar

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get('/')
+# especificando explicitamente qual código HTTP
+# esta requição deve retornar
+@app.get('/', status_code=HTTPStatus.OK)
 async def read_root() -> dict[str, str]:
     return {'message': 'Olá Mundo'}
